0ba2fc70a7ee0701489b282cd97fa76d86415b28
vricheb973 <vricheb973@g.educaand.es>
2024-03-08 14:15:29 +0100

    Subida inicial

---
+++ src/tema8/cola/ColaEnteros.java
@@ -0,0 +1,8 @@
+package tema8.cola;
+
+public interface ColaEnteros {
+	
+	void encolar(int n);
+	int desencolar();
+
+}
---
+++ src/tema8/cola/Main.java
@@ -0,0 +1,15 @@
+package tema8.cola;
+
+public class Main {
+	
+
+	public static void main(String[] args) {
+		ListaNumeros ln = new ListaNumeros();
+
+		ln.insertarPrincipio(3);
+		ln.insertarPrincipio(4);
+		ln.insertarPrincipio(5);
+		ln.insertarPrincipio(6);
+	}
+
+}
